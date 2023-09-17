ec66c9c0c5006ab837c37e01c487a30949516806
Ananda Guedes do Ó <anandaguedesdoo@gmail.com>
2023-09-17 17:12:56 -0300

    Add files via upload
    beecrowd practice

---
+++ Python/beecrowd/salario.py
@@ -0,0 +1,8 @@
+funcnum = int(input())
+htrab = int(input())
+valorh = float(input())
+
+salario = htrab * valorh
+
+print('NUMBER =', funcnum)
+print('SALARY = U$ {:.2f}'.format(salario))
---
+++ Python/beecrowd/somaSimples.py
@@ -0,0 +1,10 @@
+soma = 0
+A = int(input())
+B = int(input())
+
+if A >= 0 and B >= 0:
+  soma = (A) + (B)
+  print('SOMA =', soma)
+else:
+  soma = (A) + (B)
+  print('SOMA =', soma)
