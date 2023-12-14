efdb6c58b80824bb2f6a4e924fd282cb900d1def
Ananda Guedes <anandaguedesdoo@gmail.com>
2023-12-14 01:10:40 -0300

    estudos 14/12  1:28

--- Python/DataStructure/hashing/mainHash.py
+++ Python/DataStructure/hashing/mainHash.py
@@ -2,16 +2,18 @@ from hashTable import *
 
 ht = HashTable()
 
-ht.put(3,2)
+print(f'Hash antes da inserção de elementos: {ht}\n')
+ht.put(3,2) #utilizando o método, passando key e value
 ht.put(14,165)
 ht.put(32,5)
-print(ht.__dict__)
-ht[2] = 99
-print(ht.__dict__)
-input()
-
-hashtablemtdoida = HashTable()
-hashtablemtdoida.put(2,3)
-hashtablemtdoida.put(22,4)
-hashtablemtdoida.put(32,5)
-print(hashtablemtdoida.__dict__)
+# print(ht.__dict__)
+ht[2] = 99 #adiconar um valor a chave
+print(f'Hash após a insersão de elementos: {ht}\n')
+print(f'Diga o valor contido na chave (2): {ht.get(3)}\n')#pegue essa chave e me devolva seu valor
+ht.remove(32) #utilizando o método remove, que recebe a key e exclui o correspondente
+print(f'Hash após a remoção de elementos: {ht}\n')
+print(f'Pares de itens: {ht.items()}\n') #método que devolve os pares (key, value)
+print(f'Chaves: {ht.keys()}\n') #pega as chaves
+print(f'Valores: {ht.values()}\n') #pega os valores
+print('\nTABELA\n-----------------')
+ht.showHashTable()
