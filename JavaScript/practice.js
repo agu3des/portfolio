940bc97428218ea479659b2dd9cc6b5743ee803b
Ananda Guedes <anandaguedesdoo@gmail.com>
2024-02-09 23:00:26 -0300

    prtica com js

---
+++ JavaScript/practice.js
@@ -0,0 +1,12 @@
+var idade = 18
+var sexo = "M"
+
+console.log(idade)
+console.log(sexo)
+
+
+var idade = 19
+var nome = 'Ananda'
+var nota = 10
+console.log('A aluna '+nome +'com' +idade +'anos, tirou a nota ' +nota)
+console.log(`A aluna ${nome} com ${idade} anos, tirou a nota ${nota}`)
