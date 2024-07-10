1c972893b6700bfbba906e2dbca30d35736e7732
Ananda <anandaguedesdoo@gmail.com>
2024-07-10 14:54:45 -0300

    exclusao de class

--- out/production/portfolio/NodeJS/Calculadora/Somar.js
+++
@@ -1,5 +0,0 @@
-var sum = function(a,b){
-    return a + b;
-}
-
-module.exports = sum;
--- out/production/portfolio/Python/AulaoAutomacao/auxiliar.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import time
-
-time.sleep(5) #para definir a posiçãao do mouse
-#mostrar aa posição do mouse
-print(pyautogui.position())
