211e5555d116bc85d3313f965c1ccf94b53f1b70
Ananda Guedes do Ó <anandaguedesdoo@gmail.com>
2024-09-11 17:15:32 -0300

    remoção de arquivos desnecessários

--- Sql/Pedidos-ago.sql
+++
@@ -1,8 +0,0 @@
-select * from vendedor;
-
-insert into vendedor values(default, 'Amélia Cavalcante', '22/04/1997', 2200, 'B');
-insert into vendedor values(default, 'Rafael Junior', '17/04/2003', 2800, 'C');
-select * from vendedor;
-commit;
-insert into vendedor values(26, 'augustinho Carrara', '11/05/1973', 2800, 'C');--fica waiting
---quando o outro usuário der commit ele vai dar erro dizendo que a chave não pode ser acessada/ se o outro der rollback, dá certo
--- Sql/gerente.sql
+++
@@ -1,3 +0,0 @@
-select * from engenheiros;
-
-insert into engenheiro values(default, 'Mariazinha das Graças', 3300);
